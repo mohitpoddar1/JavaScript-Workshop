4d7ce6401e78de465c3edf8bc75cfe578c2af6ef
mohitpoddar <mohitpoddar999@gmail.com>
2024-07-02 18:37:40 +0545

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// day-1
+
+// // var bottle = "paani"
+// // let bottle = 'paani'
+// // const bottle  = 'paani'
+
+// const letter1 = 'a'
+// const letter2 = 'b'
+// const letter3 = 'c'
+
+
+// const letter = ['a','b','c',15,true]
+
+// const name = "abcd"
+// const age = 15 
+// const address = 'efgh'
+
+// const me = {
+//     name : 'abcd',
+//     age : 15, 
+//     address : 'efgh'
+// }
+
+// day-2
 
 // const data = [1,2,3,4,5,6] //js array
 // data.splice(1,3,'hello','bye','world') //To change the data of array
@@ -14,12 +38,12 @@
 
 
 // const text = 'I am from nepal' 
-// const splittedData = text.split(" ")
+// const splittedData = text.split(" ") // break the sentence by replacing with the thing written inside bracket
 // console.log(splittedData)
 
 // const language = ['js','php','c']
 // language.splice(1,0,'html')
-// //language[1]="html"
+// language[1]="html"
 // console.log(language)
 
 // const person = Object.freeze({
@@ -43,25 +67,24 @@
 // datas.name.push('sanjeev')
 // console.log(datas.name)
 
-// const datas = (
+// const datas = [
 //     {
-//     name : ['manish']
+//      name : ['manish']
 //     },
 //     {
 //      name : ['pranjal']
-//     } 
-// )
+//     }
+// ]
 
-// datas.push({name :['sanjeev']})
+// datas.push({name : ['sanjeev']})
 // console.log(datas)
 
-const datas = ({
-    name : ['manish','pranjal','sanjeev']
-})
+// const datas = ({
+//     name : ['manish','pranjal','sanjeev']
+// })
+// datas.name.splice(0,3,1,2,3)
+// console.log(datas)
 
 
-
-datas.name.splice(0,3,1,2,3)
-console.log(datas)
 
 
